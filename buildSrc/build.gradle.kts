52134805590941f538feb2f1d78fd3cb2f628965
Julio Mendoza <juliomendoza@Julios-MBP.fritz.box>
2021-02-06 21:24:26 +0100

    Registered the plugin and removed unused constants

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,16 @@ plugins {
     `kotlin-dsl`
 }
 
+gradlePlugin {
+    val appBaseModulePlugin = "app-base-module-plugin"
+    plugins {
+        register(appBaseModulePlugin) {
+            id = appBaseModulePlugin
+            implementationClass = "AndroidAppConfigurationPlugin"
+        }
+    }
+}
+
 dependencies {
     implementation("com.android.tools.build:gradle:4.1.2")
     implementation(kotlin("gradle-plugin", "1.4.30"))
